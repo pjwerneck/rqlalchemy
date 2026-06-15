f0e930e024ddf529a21d9d58f02887ed386f1d93
Pedro Werneck <pjwerneck@gmail.com>
2026-06-15 07:11:30 -0300

    Bump version: 0.6.0 → 0.6.1

--- rqlalchemy/__init__.py
+++ rqlalchemy/__init__.py
@@ -3,7 +3,7 @@ from rqlalchemy.query import RQLSelectError
 from rqlalchemy.query import select
 
 __title__ = "rqlalchemy"
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 __author__ = "Pedro Werneck"
 __license__ = "MIT"
 
